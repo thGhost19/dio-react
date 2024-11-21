25ea277d343c3557da58a5361d0a8b078bed713e
Erick Almeida <erick.almeida@fcid.com.br>
2024-11-21 00:51:29 -0300

    finalize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Content, Row } from './styles';
+import Input from './components/Input';
+import Button from './components/Button';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber((prev) => `${prev === '0' ? '' : prev}${number}`);
+  };
+
+  const handleClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
+  };
+
+  const handleOperation = (operator) => {
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation(operator);
+    } else {
+      handleEquals();
+      setOperation(operator);
+    }
+  };
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      const num1 = Number(firstNumber);
+      const num2 = Number(currentNumber);
+
+      let result = 0;
+
+      switch (operation) {
+        case '+':
+          result = num1 + num2;
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          result = num2 !== 0 ? num1 / num2 : 'Erro';
+          break;
+        default:
+          return;
+      }
+
+      setCurrentNumber(String(result));
+      setFirstNumber('0');
+      setOperation('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="*" onClick={() => handleOperation('*')} />
+          <Button label="/" onClick={() => handleOperation('/')} />
+          <Button label="C" onClick={handleClear} />
+          <Button label="CE" onClick={handleClear} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="-" onClick={() => handleOperation('-')} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="+" onClick={() => handleOperation('+')} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="=" onClick={handleEquals} />
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')} />
+          <Button label="." onClick={() => handleAddNumber('.')} />
+        </Row>
+      </Content>
+    </Container>
   );
-}
+};
 
 export default App;
